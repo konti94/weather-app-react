50ce842f79c7efe0406d207d10d488e36ba3ea18
konti <kontos.balazs.94@gmail.com>
2023-09-30 15:41:46 +0200

    app component and css modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import CitySelector from './components/CitySelector';
+import WeatherDashboard from './components/WeatherDashboard';
 
 function App() {
-	const [citySelectorIsVisible, setcitySelectorIsVisible] = useState(true);
-	const [currentCity, setCurrentCity] = useState('');
-
-	console.log(currentCity);
+	const [citySelectorIsVisible, setCitySelectorIsVisible] = useState(true);
+	const [weatherDashboardIsVisible, setWeatherDashboardIsVisible] =
+		useState(false);
+	const [currentWeather, setCurrentWeather] = useState({});
+	const [weatherForecast, setWeatherForecast] = useState({});
 
 	return (
-		<div className='App'>
-			<div className='container-sm vh-100 d-flex flex-column align-items-center justify-content-center'>
-				{citySelectorIsVisible && (
-					<CitySelector
-						setCurrentCity={setCurrentCity}
-						setcitySelectorIsVisible={setcitySelectorIsVisible}
-					/>
-				)}
+		<div>
+			<img src='' alt='' className='app-background-image d-none' />
+			<div className='container-fluid vh-100 d-flex flex-column align-items-center justify-content-center'>
+				<div className='container-sm'>
+					{citySelectorIsVisible && (
+						<CitySelector
+							setCitySelectorIsVisible={setCitySelectorIsVisible}
+							setWeatherDashboardIsVisible={
+								setWeatherDashboardIsVisible
+							}
+							setCurrentWeather={setCurrentWeather}
+							setWeatherForecast={setWeatherForecast}
+						/>
+					)}
+					{weatherDashboardIsVisible && (
+						<WeatherDashboard
+							setCitySelectorIsVisible={setCitySelectorIsVisible}
+							setWeatherDashboardIsVisible={
+								setWeatherDashboardIsVisible
+							}
+							currentWeather={currentWeather}
+							weatherForecast={weatherForecast}
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	);
